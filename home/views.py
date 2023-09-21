df45ffe42fd9b2cf1aebebaacd074a8fdc8a7467
Adhil Rahman <amazementtech521@gmail.com>
2023-09-21 12:03:08 +0530

    Hospital manage web

---
+++ home/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Departments,Doctors
+from .forms import BookingForm
+
+# Create your views here.
+def index(request):
+   return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def booking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'confirmation.html')
+    form = BookingForm()
+    dict_form = {
+        'form' : form
+    }
+    return render(request, 'booking.html', dict_form)
+
+def doctors(request):
+    dict_docs = {
+        'doctors' : Doctors.objects.all()
+    }
+    return render(request, 'doctors.html',dict_docs)
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+def department(request):
+    dict_dept = {
+        'dept': Departments.objects.all()
+    }
+    return render(request, 'department.html',dict_dept)
